f00634e2bbdd768a09c89ebd33e76f6a7a6ce8da
Aza444 <125324304+Aza444@users.noreply.github.com>
2023-03-20 17:49:27 -0600

    Add files via upload

---
+++ Base-de_datos-llamada_Almacen-main/Actividad_Asincrona_Almacen_de_productos-main/Actividad_S8_Almacen_de_productos/Program.cs
@@ -0,0 +1,43 @@
+﻿using Actividad_S8_Almacen_de_productos.Models;
+
+Console.WriteLine();
+Console.WriteLine("           ******************************************************************       ");
+Console.WriteLine("                   BIENVENIDO NUEVAMENTE A SU ALMACEN CHEPITOS :)         ");
+Console.WriteLine("           ******************************************************************       ");
+Console.WriteLine();
+
+AlmacenContext db = new AlmacenContext();
+Producto producto = new Producto();
+
+Console.WriteLine();
+
+Console.Write("- Nombre de el producto ---> ");
+producto.Nombre = Console.ReadLine();
+Console.Write("- Descripción del producto ---> ");
+producto.Descripcion = Console.ReadLine();
+Console.Write("- Precio de el producto ---> ");
+producto.Precio = Convert.ToDecimal(Console.ReadLine());
+Console.Write("- Cantidad de productos en stock ---> ");
+producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+db.Producto.Add(producto);
+db.SaveChanges();
+
+Console.WriteLine("      El producto se registro con exito   ");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine(" Id Cantidad. Productos Descripcion Precio ");
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine();
+
+var ListaProductos = db.Producto.ToList();
+foreach (var product in ListaProductos){
+
+    Console.WriteLine($"  {product.Id}  {product.Stock}    {product.Nombre} / {product.Descripcion} / ${product.Precio}");
+}
+
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine("         GRACIAS POR PREFERIR ALMACENES CHEPITOS VUELVA PRONTP :)           ");
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine();
